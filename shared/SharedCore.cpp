0ba163882c33a11cdaa1c4581e79310ecdb3bbec
Steffen Funke <info@steffen-funke.de>
2017-04-27 17:54:47 +0200

    basic opencv integration (requires android and ios opencv sdk ouside of project

--- shared/SharedCore.cpp
+++ shared/SharedCore.cpp
@@ -4,10 +4,12 @@
 //
 
 #include "SharedCore.h"
+#include <opencv2/core/core.hpp>
 
 const char* CPP_BASE_STRING = "CPP says hello world to %s";
 
 const char* concatMyStringWithCppString(const char* myString) {
+    CV_VERSION;
     char* concatenedString = new char[strlen(CPP_BASE_STRING) + strlen(myString)];
     sprintf(concatenedString, CPP_BASE_STRING, myString);
     return concatenedString;
